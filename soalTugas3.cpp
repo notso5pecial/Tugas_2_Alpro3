4bf70ad48e215c912ed47473ec07a70018aeb229
Naila <naelazure@gmail.com>
2026-03-18 19:56:17 +0700

    soalTugas3.cpp

--- soalTugas3.cpp
+++ soalTugas3.cpp
@@ -1,23 +1,30 @@
-/*
-Lembaga Kemahasiswaan USU memiliki sebuah program yang menentukan kategori beasiswa yang dapat diterima oleh mahasiswa yang mengajukan beasiswa. Penentuan beasiswa ini didasarkan pada Indeks Prestasi (IP) mahasiswa dan Gaji Orang Tua.
+#include <iostream>
+using namespace std;
 
-Ketentuan Penerimaan Beasiswa
-1. Mahasiswa dengan IP ≥ 3.5 otomatis berhak atas beasiswa kategori 4.
-2.  Mahasiswa berhak atas beasiswa kategori 1 jika:
-- Gaji orang tua kurang dari 1 juta rupiah.
-- IP mahasiswa kurang dari 3.5.
-3. Mahasiswa berhak atas beasiswa kategori 2 atau 3 jika:
-- Gaji orang tua berada di rentang 1 juta - 5 juta rupiah.
-- IP mahasiswa kurang dari 3.5:
-    - Jika IP ≥ 2.0, berhak atas beasiswa kategori 3.
-    - Jika IP < 2.0, berhak atas beasiswa kategori 2.
-4. Mahasiswa yang tidak memenuhi kriteria di atas tidak mendapatkan beasiswa.
+int main(){
+    float gaji_ortu, ipk; // deklarasi variabel untuk menyimpan gaji orang tua dan IPK
 
-Buatlah program yang membaca 2 input berupa bilangan riil (float):
-IP mahasiswa (contoh: 3.51)
-Gaji orang tua dalam juta rupiah (contoh: 1.5, yang berarti 1.5 juta rupiah)
-Program harus mencetak kategori beasiswa yang diperoleh mahasiswa sesuai dengan ketentuan di atas. Jika mahasiswa tidak berhak atas beasiswa, cetak “Tidak berhak mendapatkan beasiswa”.
+    cout<<"kelayakan menerima beasiswa"<<endl;  // menampilkan judul program
+    cout<<"masukkan gaji orang tua: "<<endl;  // input gaji orang tua
+    cin>> gaji_ortu;
+    cout<<"masukkan ipk anda: "<<endl; //input ipk mahasiswa
+    cin>> ipk;
 
-*/
+    if (ipk >= 3.5){
+        cout<<"anda berhak atas beasiswa kategori 4"; // jika IPK sangat tinggi, langsung dapat kategori 4
+    }
+    else if (gaji_ortu < 1000000 && ipk < 3.5){
+        cout<<"anda berhak atas beasiswa kategori 1";// jika gaji sangat rendah dan IPK < 3.5 → kategori 1
+    }
+    else if (gaji_ortu <= 5000000 && ipk >= 2.0){
+        cout<<"anda berhak atas beasiswa kategori 3";// jika gaji menengah dan IPK cukup (>=2.0) → kategori 3
+    }
+    else if (gaji_ortu <=5000000 && ipk < 2.0){
+        cout<<"anda berhak atas beasiswa kategori 2";// jika gaji menengah tapi IPK rendah → kategori 2
+    }
+    else{
+        cout<<"tidak berhak mendapatkan beasiswa";// selain kondisi di atas → tidak dapat beasiswa
+    }
+    system ("pause");//menahan layar console supaya tidak langsung tertutup
 
-// Hapus komentar yang berisi soal sebelum memasukkan kode untuk jawaban
+}
